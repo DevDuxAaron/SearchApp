f42da0bbce36accb473b9b101fa681793107f00c
DevDuxAaron <relojvale25@gmail.com>
2021-01-17 17:16:51 -0400

    Clase finalizada, web app para la busqueda en wikipedia

---
+++ js/main.js
@@ -0,0 +1,43 @@
+import { clearPushListener, clearSearchText, setSearchFocus, showClearTextButton } from "./searchBar.js"
+import { deleteSearchResults, buildSearchResults, clearStatsLine, setStatsLine } from "./searchResults.js"
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js"
+
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete")
+        initApp();
+});
+
+const initApp = () => {
+    setSearchFocus();
+    // set the focus
+
+    // 3 listeners clear text
+    const search = document.getElementById("search")
+    search.addEventListener("input", showClearTextButton)
+    const clear = document.getElementById("clear")
+    clear.addEventListener("click", clearSearchText)
+    clear.addEventListener("keydown", clearPushListener)
+    const form = document.getElementById("searchBar")
+    form.addEventListener("submit", submitTheSearch);
+}
+
+const submitTheSearch = (event) => {
+    event.preventDefault();
+    // delete search results
+    deleteSearchResults()
+    // process the search
+    processTheSearch();
+    // set the focus
+    setSearchFocus();
+}
+
+const processTheSearch = async () => {
+    // claer stats line
+    clearStatsLine()
+    const searchTerm = getSearchTerm();
+    if (searchTerm === "") return;
+    const resultArray = await retrieveSearchResults(searchTerm)
+    if (resultArray.length) buildSearchResults(resultArray);
+    setStatsLine(resultArray.length)
+}
